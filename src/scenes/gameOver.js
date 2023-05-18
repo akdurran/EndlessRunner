ebd99a75a00d37d89a87d206128a49e58707a61e
Areeb Durrani <akdurran@ucsc.edu>
2023-05-18 13:20:46 -0700

    added four more sound effects

--- src/scenes/gameOver.js
+++ src/scenes/gameOver.js
@@ -2,11 +2,28 @@ class GameOver extends Phaser.Scene {
     constructor() {
         super("gameOverScene");
     }
+
+    preload() {
+    this.load.audio('sfx_explosion', './assets/explosion38.wav');
+    this.load.audio('sfx_explosion2', './assets/explosion.wav');
+    this.load.audio('sfx_explosion3', './assets/explosion(1).wav');
+    this.load.audio('sfx_explosion4', './assets/explosion(2).wav');
+    this.load.audio('sfx_explosion5', './assets/explosion(3).wav');
+    }
     create() {
+
+      this.soundPlayed = false;
+
+      this.explosionSounds = ['sfx_explosion', 'sfx_explosion2', 'sfx_explosion3', 'sfx_explosion4',
+      'sfx_explosion5'];
+
+      
 
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
 
         this.sound.stopAll();
+
+        
 
         let menuConfig = {
             fontFamily: 'Courier',
@@ -38,6 +55,10 @@ class GameOver extends Phaser.Scene {
           this.add.text(game.config.width/2, 20, 'HIGH SCORE: ' + highScore, highScoreConfig);
     }
     update() {
+      if(!this.soundPlayed) {
+        this.sound.play(this.explosionSounds[Phaser.Math.Between(0, 4)]);
+        this.soundPlayed = true;
+      }
         if (Phaser.Input.Keyboard.JustDown(keyF)) {
             increment = 0;
            this.scene.start('playScene');
